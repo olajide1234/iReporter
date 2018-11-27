4b6e504dacf47ac18cb0905f49000426b5faeea4
Olajide <ayinlaolajide@yahoo.com>
2018-11-27 20:45:02 +0100

    Add API feature to enable user get a specific red-flag record
    Enable user get a specific red-flag record via API

---
+++ index.js
@@ -0,0 +1,77 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import es6mapimplement from 'es6-map/implement';
+
+// Data model
+const redFlagRecords = new Map([
+  [1, {
+    id: 1,
+    createdOn: 'Sample date',
+    createdBy: 'Sample Creator', // represents the user who created this record
+    type: 'Sample record type', // [red-flag, intervention]
+    dateOfIncident: 'Sample date',
+    title: 'Sample title',
+    comment: 'Sample comment',
+    Images: ['Sample Image'],
+    Videos: ['Sample Video'],
+    location: 'Sample record location', // Lat Long coordinates
+    status: 'Sample status', // [draft, under investigation, resolved, rejected]
+  }],
+]);
+
+const userList = new Map([
+  ['admin', {
+    id: 'Integer',
+    firstname: 'String',
+    lastname: 'String',
+    othernames: 'String',
+    email: 'String',
+    phoneNumber: 'String',
+    username: 'admin',
+    registered: 'Date',
+    isAdmin: true,
+    password: 'encoded password',
+  }],
+]);
+
+// Set up the express app
+const app = express();
+
+//  Setup bodyParser
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Get all red-flag records
+app.get('/api/v1/red-flags', (req, res) => {
+  const allRedFlagRecords = [...redFlagRecords.values()];
+  res.status(200).send({
+    status: 200,
+    data: allRedFlagRecords,
+  });
+});
+
+//  Get a single red-flag record
+app.get('/api/v1/red-flags/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if ([...redFlagRecords.keys()].includes(id) && redFlagRecords.get(id) !== null) {
+    return res.status(200).send({
+      status: 200,
+      data: [redFlagRecords.get(id)],
+    });
+  }
+  return res.status(404).send({
+    status: 404,
+    error: 'Not found: Record not found',
+  });
+});
+
+// Set up port
+const PORT = process.env.PORT || 2000;
+
+//  Diabled so as not to conflict mocha --watch.
+//  Solution ref: http://www.marcusoft.net/2015/10/eaddrinuse-when-watching-tests-with-mocha-and-supertest.html
+// if (!module.parent) {
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
